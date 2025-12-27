363fe7966f1203ecb1c4d058bc4f998daeb3f225
Soda-Lynn <akira.kai.272@gmail.com>
2025-12-27 11:01:18 +0630

    Update publish.js

--- functions/publish.js
+++ functions/publish.js
@@ -1,23 +1,21 @@
 export async function onRequestPost({ request, env }) {
-  try {
-    const data = await request.json();
-    if (!data.content) {
-      return new Response("No content", { status: 400 });
-    }
+  const data = await request.json();
 
-    const id = Math.random().toString(36).slice(2, 8);
-
-    await env.POSTS.put(id, JSON.stringify({
-      title: data.title,
-      content: data.content,
-      ogImage: data.ogImage || ""
-    }));
-
-    return new Response(JSON.stringify({ url: `/p/${id}` }), {
-      headers: { "Content-Type": "application/json" }
-    });
-
-  } catch (e) {
-    return new Response(e.message, { status: 500 });
+  if (!data.content) {
+    return new Response("No content", { status: 400 });
   }
+
+  const id = Math.random().toString(36).slice(2, 8);
+
+  await env.POSTS.put(id, JSON.stringify({
+    title: data.title || "NUCS Diary",
+    content: data.content,
+    ogImage: data.ogImage || ""
+  }));
+
+  return new Response(JSON.stringify({
+    url: `/p/${id}`
+  }), {
+    headers: { "Content-Type": "application/json" }
+  });
 }
